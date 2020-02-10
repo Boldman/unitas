b940430a536077cbf826f70130848878038bf6fa
Jacob Boldman <jacob@boldman.co>
2020-02-10 15:50:55 -0800

    temporarily changed to NixOS version 19.03

--- hosts/iroas.nix
+++ hosts/iroas.nix
@@ -8,7 +8,7 @@
   };
 
   system = {
-    stateVersion = "19.09";
+    stateVersion = "19.03";
     autoUpgrade.channel = "https://nixos.org/channels/nixos-19.09";
   };
   nix.maxJobs = lib.mkDefault 16;
