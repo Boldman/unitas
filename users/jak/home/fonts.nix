85b2a3575530c817e8a084c079e76ef117714bfb
Jacob Boldman <jak@boldman.co>
2020-03-24 21:16:59 -0700

    added configs for command-not-found, manpages, and fonts

---
+++ users/jak/home/fonts.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+
+# This file installs fonts and enables fontconfig.
+
+{
+
+  home.packages = lib.lists.optionals (!config.veritas.david.dotfiles.headless) (
+    with pkgs; [
+      meslo-lg
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
+      font-awesome_5
+      inconsolata
+      siji
+      material-icons
+      powerline-fonts
+      roboto
+      roboto-mono
+      roboto-slab
+      iosevka
+    ]
+  );
+}
+
+# vim:foldmethod=marker:foldlevel=0:ts=2:sts=2:sw=2:nowrap
